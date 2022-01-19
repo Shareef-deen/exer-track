dcb80c7796a91ca8d2f56cefd82a0524103e60fa
Shareef-deen <49625061+Shareef-deen@users.noreply.github.com>
2022-01-19 11:45:41 +0000

    Update server.js

--- server.js
+++ server.js
@@ -76,9 +76,10 @@ actual.findOneAndUpdate({_id: id},{$set:{ description: des, duration: dur, date:
         "date":date,
         "duration":dur,"description": des
 }*/
-     res.json({username: data.username, "description": des, "duration": parseInt(dur), "date": date,"_id": id});
-  
-})
+
+         res.json({"username": data.username, "description": des, "duration": parseInt(dur), "date": date,"_id": id});
+    
+  })
 /*multi.create({userid: id, description: des, duration: dur, date: date}, (err, data)=>{
   if (err) return console.error(err);
   else{
